069cdead0510cfeced2972b7b84d6e33db02701f
ammun-re <omar.karray.pro@gmail.com>
2017-06-30 22:09:15 +0200

    Initial

---
+++ plugins/amunre/events/Plugin.php
@@ -0,0 +1,14 @@
+<?php namespace AmunRe\Events;
+
+use System\Classes\PluginBase;
+
+class Plugin extends PluginBase
+{
+    public function registerComponents()
+    {
+    }
+
+    public function registerSettings()
+    {
+    }
+}
